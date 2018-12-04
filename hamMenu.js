0f5925d7dc00dddead22ad5526cce7fca19151a5
dawid <dawidgardzi@onet.pl>
2018-12-04 14:10:22 +0100

    v016

--- hamMenu.js
+++ hamMenu.js
@@ -1,20 +1,3 @@
-/*const btn = document.getElementById('hamburgerMenu');
-btn.addEventListener('click', hamMenu)
-btn.addEventListener('focusout', hamMenu)
-
-
-
-function hamMenu(){
-    const menu = document.getElementById('menu');
-
-    if(document.activeElement === btn )
-    {
-        menu.style.display = 'block';
-    } else {
-        menu.style.display = 'none';
-    }
-}*/
-
 const btn = document.getElementById('hamburgerMenu');
 btn.addEventListener('click', hamMenu)
 btn.addEventListener('focusout', hamMenu)
