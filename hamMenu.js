999bd77da10bfd10efe0b26a1088a1b0b569c920
dawid <dawidgardzi@onet.pl>
2018-12-04 13:26:35 +0100

    v015

--- hamMenu.js
+++ hamMenu.js
@@ -1,4 +1,4 @@
-const btn = document.getElementById('hamburgerMenu');
+/*const btn = document.getElementById('hamburgerMenu');
 btn.addEventListener('click', hamMenu)
 btn.addEventListener('focusout', hamMenu)
 
@@ -12,5 +12,31 @@ function hamMenu(){
         menu.style.display = 'block';
     } else {
         menu.style.display = 'none';
+    }
+}*/
+
+const btn = document.getElementById('hamburgerMenu');
+btn.addEventListener('click', hamMenu)
+btn.addEventListener('focusout', hamMenu)
+const menu = document.getElementById('menu');
+const mobileViewport = window.matchMedia("(max-width: 840px)");
+
+
+function hamMenu() {
+    if (document.activeElement === btn) {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+
+}
+
+mobileViewport.addListener(showMenu);
+
+function showMenu() {
+    if (mobileViewport.matches) {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
     }
 }
